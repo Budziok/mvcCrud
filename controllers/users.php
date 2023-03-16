415d2d3d59b53d5bb31fe5ffb764db8082ed2c1c
Krzysztof Budzik <kbudzik224@gmail.com>
2023-03-16 15:54:38 +0100

    login added

--- controllers/users.php
+++ controllers/users.php
@@ -26,4 +26,32 @@ class UsersController extends Controller
         }
 
     }
+
+    public function login()
+    {
+        $this->returnView('login');
+    }
+
+    public function authenticate()
+    {
+        $model = new User();
+
+        if($model->login())
+        {
+            Messages::setMessage("Zalogowano", "success");
+            $this->redirect('home');
+        } else {
+            Messages::setMessage("Nie udało się zalogować", "error");
+            $this->redirect('users','login'); 
+        }
+
+    }
+
+    public function logout()
+    {
+            unset($_SESSION['is_logged_in']);
+            unset($_SESSION['user_data']);
+            Messages::setMessage("Wylogoano","success");
+            header('Location:' .ROOT_URL);
+    }
 }
--- models/user.php
+++ models/user.php
@@ -27,5 +27,31 @@ class User extends Model
 		return false;
 	}
 
-    // public function login() {}
+    public function login() {
+		
+		$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+		//$password = $this->hashPassword($post['password'], $post['email']);
+
+		if($post['submit']) {
+
+			$this->query('SELECT * FROM users WHERE email = :email');
+			$this->bind(':email', $post['email']);
+			
+			$row = $this->single();
+
+			if($row) {
+				if (password_verify($post['password'], $row['password'])) {
+					$_SESSION['is_logged_in'] = true;
+					$_SESSION['user_data'] = array(
+						"id"	=> $row['id'],
+						"name"	=> $row['name'],
+						"email"	=> $row['email']
+					);
+				return true;
+				}
+			}
+		}
+		return false;
+	}
 }
